dd0e1abba33527f3a036c3935cc0a002bf445096
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-24 21:10:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,10 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(1,2,3,4,2,1 ,value_to_find)
+  index(3)
+  #=>4
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value(,2,3,5,6,8,6,7,5)
+   Add your solution here
 end
 
 def find_min_value(array)
